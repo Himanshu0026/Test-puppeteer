f998eddf78f583c6cea458343855164b03d74222
sangita@websitetoolbox.com <sang@123>
2016-06-02 18:38:41 +0530

    Add test script for login functionality with all scenarios and loginData json file

--- forum/testsuite/forum_login.js
+++ forum/testsuite/forum_login.js
@@ -3,48 +3,103 @@
 
 var casper = require('casper').create({
 	verbose : true,
- 	logLevel : "debug",
+ 	//logLevel : "debug",
 	viewportSize: { width: 1024, height: 768 }
 });
 
-var json = require('../testdata/logindata.json');
+var json = require('../testdata/loginData.json');
 var reusable = require('ReusableFn.js');
-require('utils').dump(json);
 
-casper.start(json.url, function() {
+casper.start(json['url'], function() {
 	this.echo("Title of the page :"+this.getTitle());
 });
 
-
+//**************************************************************************
 casper.then(function(){
-	reusable.loginToApp(json.username, json.password, casper, function(){
-		console.log("user has successfully login to application with valid username and password");
+	reusable.loginToApp(json['InvalidPassowrd'].username, json['InvalidPassowrd'].password, casper, function(){
+		console.log("*****login with valid username and invalid password and verify error message*****");
 	});
-
+});
+casper.wait(5000);
+casper.then(function() {
+var errorMessage = this.fetchText('form[name="frmLogin"] [role="alert"]');
+this.echo(errorMessage.indexOf(json['InvalidPassowrd'].ExpectedErrorMessage));
+    	this.echo(errorMessage);
+	if(errorMessage.indexOf(json['InvalidPassowrd'].ExpectedErrorMessage)>=0){
+		this.echo('Error message is verified when user try to login with invalid password');
+	}else{
+		console.log("Error occurred");
+		this.capture("ScreenShots/ErrorOnInvalidPassword.png");
+	}
 });
 
-casper.wait(7000, function() {
-	this.capture('ScreenShots/login.png');
-});
+//************************************************************************************
 
+casper.thenOpen(json['url']);
 casper.then(function(){
-	reusable.gotoNewTopicpage(casper,function(){
-		console.log("redirect to new topic");
+	reusable.loginToApp(json['InvalidUsername'].username, json['InvalidUsername'].password, casper, function(){
+		console.log("*****login with invalid username and password and verify error message*****");
 	});
 });
-
-casper.wait(7000, function(){
-	this.capture('ScreenShots/newTopic.png');
+casper.wait(5000);
+casper.then(function() {
+var errorMessage = this.fetchText('form[name="frmLogin"] [role="alert"]');
+	this.echo(errorMessage);
+	if(errorMessage.indexOf(json['InvalidUsername'].ExpectedErrorMessage)>=0){
+		this.echo('Error message is verified when user try to login with invalid username');
+	}else{
+		console.log("Error occurred");
+		this.capture("ScreenShots/Error_ErrorOnInvalidUsername.png");
+	}
 });
 
-casper.then(function(){console.log("===================json.title : "+json.title+ " json.content : "+json.content +" json.category : "+json.category);
-	reusable.postTopicpage(json.title, json.content, json.category, casper, function(){
-		console.log("Topic Posted");
+//************************************************************************************
+
+casper.thenOpen(json['url']);
+casper.then(function(){
+	reusable.loginToApp(json['BlankField'].username, json['BlankField'].password, casper, function(){
+		console.log("*****login by leaving blank username and password and verify error message*****");
 	});
 });
+casper.wait(5000);
+casper.then(function() {
+var errorMessage = this.fetchText('form[name="frmLogin"] [role="alert"]');
+	this.echo(errorMessage);
+	if(errorMessage.indexOf(json['BlankField'].ExpectedErrorMessage)>=0){
+		this.echo('Error message is verified when user try to login with blank data');
+	}else{
+		console.log("Error occurred");
+		this.capture("ScreenShots/Error_LoginwithBlankfield.png");
+	}
+});
 
-casper.wait(7000,function(){
-	this.capture("ScreenShots/postedTopic.png");
+//************************************************************************************
+
+casper.thenOpen(json['url']);
+casper.then(function(){
+	reusable.loginToApp(json['BlankPassword'].username, json['BlankPassword'].password, casper, function(){
+		console.log("*****login by leaving blank username and password and verify error message*****");
+	});
+});
+casper.wait(5000);
+casper.then(function() {
+var errorMessage = this.fetchText('form[name="frmLogin"] [role="alert"]');
+	this.echo(errorMessage);
+	if(errorMessage.indexOf(json['BlankPassword'].ExpectedErrorMessage)>=0){
+		this.echo('Error message is verified when user try to login with blank password');
+	}else{
+		console.log("Error occurred");
+		this.capture("ScreenShots/Error_LoginwithBlankPassword.png");
+	}
+});
+
+//**************************************************************************************
+
+casper.thenOpen(json['url']);
+casper.then(function(){
+	reusable.loginToApp(json['ValidCredential'].username, json['ValidCredential'].password, casper, function(){
+		console.log("User has been successfuly login to application");
+	});
 });
 
 casper.then(function() {
@@ -53,8 +108,6 @@ casper.then(function() {
 	});
 });
 
-casper.wait(7000, function() {
-	this.capture('Screenshots/logout.png');
-});
+//****************************************************************************************
 
 casper.run();
