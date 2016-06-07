013287aa04c6ad5acf942eebf6d419f8c029db71
salinsunny2 <rajat.sunny2@gmail.com>
2016-06-07 12:36:19 +0530

    working on reply post topic

--- forum/testsuite/ReusableFn.js
+++ forum/testsuite/ReusableFn.js
@@ -29,6 +29,74 @@ exports.gotoNewTopicpage = function(driver, callback) {
 
 };
 
+//method to post New Topic 
+exports.postTopicpage = function(title, content, category, driver, callback){
+/*driver.fill('form[name="PostTopic"]',{
+		'subject' : title,
+	},false);*/
+	driver.sendKeys('input[name="subject"]', title);
+
+	 driver.withFrame('message_ifr', function() {
+		this.echo("enter message in iframe");
+ 		this.sendKeys('#tinymce', content);
+		this.echo("**************************************");
+		driver.capture('Screenshots/content.png');	
+	});
+
+	driver.click('#all_forums_dropdown')
+	driver.fill('form[name="PostTopic"]',{
+		'forum' : category
+	},false);
+	//driver.sendKeys('input[name="forum"]', category);
+	driver.click('#insert_image_dialog_');
+    	driver.then(function() {
+		this.waitForSelector('div#insertimagemodal', function() {
+			this.echo('****************************************see the modal!');
+			this.wait(7000, function() {
+				this.capture('Screenshots/screenshotofmodal.png');
+			});
+		});
+	});
+	driver.then(function(){
+		driver.click('div#uploadImage a#insertImage_');
+		this.wait(10000,function(){
+			this.capture('Screenshots/browse.png');
+		});	
+	});
+	/*driver.then(function(){
+		this.page.uploadFile('input[type="file"]', '/home/rajatk/Pictures/JavascriptPart1.png');
+        	this.click('#insert_image_btn');
+	});*/
+
+	driver.then(function(){
+		this.click('#previewpost_sbt');
+		this.wait(10000,function(){
+			driver.capture('Screenshots/previewPost.png');
+		});
+	});
+	driver.then(function(){
+		driver.click('#post_submit');
+	});
+	
+	return callback();
+};
+
+// method to check posted new topic
+exports.checkPostTopicContent = function(content, driver, callback){
+console.log("#############################################################################################################");
+	var contentMsg = driver.fetchText('div.post-body-content span');
+		driver.echo('************ contentMsg : '+contentMsg.length);
+		driver.echo('************ content : '+content.length);
+		if(contentMsg.trim() == content.trim()){
+			casper.log("Successfully content verified", 'error');
+		} else {
+			casper.log("Error occured in verifying content", 'error');
+			driver.capture('contentError.png');
+		}
+		return callback();
+};
+
+
 //method for clicking on register link
 exports.clickOnRegisterLink = function(driver, callback) {
 	driver.click('.pull-right a');
@@ -47,3 +115,101 @@ exports.registerToApp = function(username, email, password, signature, driver, c
 	driver.click('form[action="/register/create_account"] button');
 	return callback();
 };
+
+ exports.gotoNewPollpage = function(driver, callback) {
+	driver.click('a[href="/post/printadd"]');
+	//driver.click('form[name="PostTopic"]');
+	driver.then(function() {
+		//this.wait(7000, function() {
+			this.capture('Screenshots/TopicDetails.png');
+			this.click('ul li a[href="#poll"]');
+		//});
+	});
+	//driver.capture('Screenshots/TopicDetails.png');
+	
+	return callback();
+};
+
+exports.savePollPage = function(pollQues, publicCheckbox, option1, option2, multiple, timeoutFormate, driver, callback){
+	driver.sendKeys('#poll_question',pollQues);
+	driver.sendKeys('input[name="public"]',publicCheckbox);
+	driver.sendKeys('#poll_option_1 div input',option1);
+	driver.sendKeys('#poll_option_2 div input',option2);
+	driver.capture("Screenshots/savePoll.png");
+	driver.wait(5000,function(){
+		driver.click('a[href="#poll-timeout"]');
+	});
+};
+
+exports.replyTopic = function(content, driver, callback){
+	driver.click('form[name="posts"] h4 a');
+	
+	/*driver.withFrame('message_ifr', function() {
+		this.echo("enter message in iframe");
+ 		this.sendKeys('#tinymce', content);
+		this.echo("**************************************");
+		driver.capture('Screenshots/replyContent.png');	
+	});*/
+	driver.then(function(){
+		driver.sendKeys('#message',content);
+	});
+	driver.wait(7000,function(){
+		driver.withFrame('message_ifr', function() {
+		this.echo("enter message in iframe");
+ 		this.sendKeys('#tinymce', content);
+		this.echo("**************************************");
+		driver.capture('Screenshots/replyContent.png');	
+	});
+	});
+
+	driver.then(function(){
+		driver.click('#reply_submit');
+	});
+	callback();
+};
+
+exports.checkPostReply = function(content, driver, callback){
+console.log("#############################################################################################################");
+	
+
+var contentMsg = driver.getElementInfo('div.post-body-content:nth-last-child(2)').text;
+
+//var contentMsg = driver.fetchText('div.post-body-content');
+	/*for(var i=0;i<contentMsg.length;i++){
+		console.log("\n"+contentMsg[i]);
+	}*/
+	/*console.log("gggbvfsdvsdsvsvsvdsvsvsvs : "+contentMsg[0]);
+	console.log("gggbvfsdvsdsvsvsvdsvsvsvs : "+contentMsg[1]);
+	console.log("gggbvfsdvsdsvsvsvdsvsvsvs : "+contentMsg[2]);
+	console.log("gggbvfsdvsdsvsvsvdsvsvsvs : "+contentMsg[10]);*/
+	driver.echo('************ contentMsg : '+contentMsg);
+	driver.echo('************ content : '+content);
+	if(contentMsg.trim() == content.trim()){
+		casper.log("Successfully content verified", 'error');
+	} else {
+		casper.log("Error occured in verifying content", 'error');
+		driver.capture('contentError.png');
+	}
+	return callback();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
