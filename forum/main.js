ba8e966792cd755ef47a784d16006da684cf1537
its4monika <monika@websitetoolbox.com>
2016-06-09 10:29:39 +0530

    Modified main.js

--- forum/main.js
+++ forum/main.js
@@ -2,10 +2,31 @@ var config = require("./config/config.json");
 
 var casper = require('casper').create(config.app);
 
-casper.echo("Casper CLI passed args:");
-require("utils").dump(casper.cli.args);
+var testArg = "";
+if(casper.cli.args.length>0)
+	testArg = casper.cli.args[0];
 
-var forumLogin = require("./testsuite/forum_login.js");
-forumLogin.featureTest(casper);
+casper.echo("Casper CLI passed args: " + testArg);
 
-casper.run();
+
+switch (testArg) {
+    case "login":
+        var forumLogin = require("./testsuite/forum_login.js");
+	forumLogin.featureTest(casper);
+	casper.run();
+        break;
+    case "register":
+        
+        break;
+    case "newtopic":
+        
+        break;
+    case "postreply":
+        
+        break;
+    default:
+        casper.echo("help");
+};
+
+
+
