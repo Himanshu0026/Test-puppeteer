6b26defe0962b9ecfc93647f1c325eefd5ae5938
its4monika <monika@websitetoolbox.com>
2016-06-09 11:34:53 +0530

    Added help instructions

--- forum/main.js
+++ forum/main.js
@@ -3,11 +3,13 @@ var config = require("./config/config.json");
 var casper = require('casper').create(config.app);
 
 var testArg = "";
-if(casper.cli.args.length>0)
+if(casper.cli.args.length>0){
 	testArg = casper.cli.args[0];
-
-casper.echo("Casper CLI passed args: " + testArg);
-
+	casper.echo("Started testing for the feature: " + testArg +"\n");
+}else{
+	casper.echo("It seems, you have not given any option.");
+	casper.echo("Please select any feature from options given below. For ex: casperjs main.js <option>.\n"); 
+}
 
 switch (testArg) {
     case "login":
@@ -25,7 +27,13 @@ switch (testArg) {
         
         break;
     default:
-        casper.echo("help");
+        casper.echo("Options:");
+	casper.echo("	register");
+	casper.echo("	login");
+	casper.echo("	newtopic");
+	casper.echo("	postreply\n");
+	casper.echo("Relevant test data has to be fed in JSON format in files placed for each feature in '<current directory>/testData/'.");
+	casper.exit();
 };
 
 
