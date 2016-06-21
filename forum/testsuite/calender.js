226a98029f680bd1c4f1657b4c4ad9051900d9d9
sangita@websitetoolbox.com <sang@123>
2016-06-21 17:49:36 +0530

    Added code for verify error message while creating new event

--- forum/testsuite/calender.js
+++ forum/testsuite/calender.js
@@ -6,12 +6,12 @@ var forumRegister = require('./register.js');
 var forumLogin = require('./forum_login.js');
 var utils = require('./utils.js');
 var json = require('../testdata/calenderData.json');
+	var screenShotsDir = config.screenShotsLocation + "calender/";
 
 var calender = module.exports = {};
 
 calender.featureTest = function(casper, test,x) {
 	
-	var screenShotsDir = config.screenShotsLocation + "calender/";
 	var moment = require('moment');
 	moment().format();
 	
@@ -24,10 +24,11 @@ calender.featureTest = function(casper, test,x) {
 		casper.echo("Current date is : "+ moment().date());
 		casper.echo("Current year is : "+ moment().year());
 		
+		
 	});
 	
 	//login to backend app by call login function
-	casper.then(function() {
+	/*casper.then(function() {
 
 		forumRegister.loginToForumBackEnd(config["backendCred"], casper, function() {
 			casper.echo(config["backendCred"].uname +"*******"+config["backendCred"].upass);
@@ -51,7 +52,7 @@ calender.featureTest = function(casper, test,x) {
 				this.capture(screenShotsDir+'updatecalenderonGeneralSetting.png');
 			});
 		});
-	});
+	});*/
 	
 	//Now login to forum and check calender option is not visible in side menu
 	casper.thenOpen(config.url);
@@ -59,17 +60,17 @@ calender.featureTest = function(casper, test,x) {
 		forumLogin.loginToApp(json.username, json.password, casper, function(){
 			casper.echo("User has successfully login to the forum application", 'info');
 		});
-		this.waitForSelector('#links-nav', function(){
+		/*this.waitForSelector('#links-nav', function(){
 			this.click('#links-nav');
 			this.wait(5000, function(){
 				test.assertDoesntExist('#calenders_show');
 				this.capture(screenShotsDir+'CalendernotVisible.png');
 			});		
-		});
+		});*/
 	});
 
 	//enable calender checkbox from general setting page in backend
-	casper.thenOpen(config.backEndUrl, function(){
+	/*casper.thenOpen(config.backEndUrl, function(){
 		casper.echo("Title of the page : "+this.getTitle(), 'info');
 	});
 	casper.then(function() {
@@ -128,14 +129,16 @@ calender.featureTest = function(casper, test,x) {
 			this.click('button[type="submit"]');
 			casper.echo("Calender related setting has been updtaed for registered user fron edit User permission page");
 			this.wait(3000, function(){
+				var updationmsg = this.fetchText('p[align="center"]');
 				test.assertExists('p[align="center"]');
+				test.assertEquals(updationMsg, 'Your user group settings have been updated.');
 				this.capture(screenShotsDir+'updateCalenderpermission.png');
 			});
 		});
 
 	});
 
-	casper.thenOpen(config.url);
+	casper.thenOpen(config.url);*/
 	//verify calender page and highlighted current date
 	casper.then(function(){
 		calender.gotoCalanderpage(casper, function() {
@@ -153,7 +156,7 @@ calender.featureTest = function(casper, test,x) {
 	});
 
 	//Verify calender mode
-	casper.then(function() {
+	/*casper.then(function() {
 
 		//verify default calender view mode after navigating to Calender page
 		this.waitForSelector('#monthlyView', function() {
@@ -252,12 +255,54 @@ calender.featureTest = function(casper, test,x) {
 			});
 		});
 
-	});	
+	});	*/
 	
 		
 	casper.then(function() {
 		this.click('a.btn.btn-sm.btn-primary');	
-		
+		this.eachThen(json['invalidInfo'], function(response) {
+			casper.echo("=========" +JSON.stringify(response.data));
+			calender.createEvent(response.data, casper, function() {
+				var responseData = response.data;
+				if(responseData.validationType == "Blank Title") {
+					casper.wait(3000, function() {
+						var ActualErrMessage = casper.fetchText('div.alert.alert-danger');
+						casper.echo("Error message in case of blank title is : "+casper.fetchText('div.alert.alert-danger'));
+						casper.capture(screenShotsDir+'createEventablankTitle.png');
+						test.assertEquals(ActualErrMessage, responseData.ExpectedErrMessage);
+					});
+					
+				}else if(responseData.validationType == "Blank Description") {
+					casper.wait(3000, function() {
+						var ActualErrMessage = casper.fetchText('div.alert.alert-danger');
+						casper.echo("Error message in case of blank description is : "+casper.fetchText('div.alert.alert-danger'));
+						casper.capture(screenShotsDir+'createEventBlankdesc.png');
+						test.assertEquals(ActualErrMessage, responseData.ExpectedErrMessage);
+					});
+				}else if(responseData.validationType == "Allday with Previous Date") {
+					casper.wait(3000, function() {
+						var ActualErrMessage = casper.fetchText('div.alert.alert-danger');
+						casper.echo("Error message for allDay with Previous date is : "+casper.fetchText('div.alert.alert-danger'));
+						casper.capture(screenShotsDir+'createEventallDay_prevDate.png');
+						test.assertEquals(ActualErrMessage, responseData.ExpectedErrMessage);
+					});
+				}else if(responseData.validationType == "Start date greater than end Date") {
+					casper.wait(3000, function() {
+						var ActualErrMessage = casper.fetchText('div.alert.alert-danger');
+						casper.echo("Error message for end date older than Start date is : "+casper.fetchText('div.alert.alert-danger'));
+						casper.capture(screenShotsDir+'createEvent_greaterStartDate.png');
+						test.assertEquals(ActualErrMessage, responseData.ExpectedErrMessage);
+					});
+				}else if(responseData.validationType == "Start date older than current Date") {
+					casper.wait(3000, function() {
+						var ActualErrMessage = casper.fetchText('div.alert.alert-danger');
+						casper.echo("Error message for Start date older than current Date is : "+casper.fetchText('div.alert.alert-danger'));
+						casper.capture(screenShotsDir+'createEvent_OldCurrentDate.png');
+						test.assertEquals(ActualErrMessage, responseData.ExpectedErrMessage);
+					});
+				}
+			});
+		});
 
 
 
@@ -306,38 +351,95 @@ calender.gotoCalanderpage = function(driver, callback) {
 
 //function to create an event
 calender.createEvent = function(data, driver, callback) {
-	driver.fill( 'form#PostCalEvent',  {
-		"event_title" : "data.title",
-		"message" : data.description
-	}, false);
 	
-	if (data.Allday){
-		utils.enableorDisableCheckbox('allDay', true, casper, function() {
-				casper.echo("All day checkbox has been enabled", 'info');
+	driver.sendKeys('input[name="event_title"]', data.Title, {reset:true});
+	driver.withFrame('message_ifr', function() {
+		this.sendKeys('#tinymce', casper.page.event.key.Ctrl,casper.page.event.key.A, {keepFocus: true});
+		this.sendKeys('#tinymce', casper.page.event.key.Backspace, {keepFocus: true});
+ 		this.sendKeys('#tinymce', data.description);	
+	});
+	driver.wait(3000, function() {
+		
+		if (data.Allday){
+			utils.enableorDisableCheckbox('allDay', true, driver, function() {
+				casper.echo("Allday checkbox has been enabled", 'info');
 			});
-	} else {
-		driver.sendKeys('#event_start_date', data.startDate);
-		driver.sendKeys('#event_end_date', data.endDate);
-		driver.click('#from_time');
-		driver.fill('form#PostCalEvent',{
-			'from_time' : data.startTime
-		},false);
-		driver.click('select[name="to_time"]');
-		driver.fill('form#PostCalEvent',{
-		'to_time' : data.endTime
-		},false);
-		driver.sendKeys('', data.startTime);
-		driver.sendKeys('', data.endTime);
+			if(!data.startDate){
+				this.sendKeys('#event_start_date', "1/23/2017", {reset:true});
+			} else {
+				this.sendKeys('#event_start_date', data.startDate, {reset:true});}
+		} else {
+			utils.enableorDisableCheckbox('allDay', false, driver, function() {
+				casper.echo("Allday checkbox has been disabled", 'info');
+			});
+			this.sendKeys('#event_start_date', data.startDate, {reset:true});
+			this.sendKeys('#event_end_date', data.endDate, {reset:true});
+			this.click('#from_time');
+			this.fill('form#PostCalEvent',{
+				'from_time' : data.startTime
+			},false);
+			this.click('select[name="to_time"]');
+			this.fill('form#PostCalEvent',{
+			'to_time' : data.endTime
+			},false);
 
-	};
+		};
+	});
 
-	if (data.Repeat) {
+	/*if (data.Repeat) {
 		utils.enableorDisableCheckbox('repeat', true, casper, function() {
 				casper.echo("Repeat checkbox has been enabled", 'info');
 			});
-	};
-
+		switch (data.recurrence) {
 		
+		case "Daily" :
+			driver.click('input#rb_event_1');
+			driver.sendKeys('input[name="weeklybox"]', data.days, {reset:true} );
+		break;
+		case "Weekly" : 
+			driver.click('input#rb_event_2');
+			driver.sendKeys('input[name="weeklybox"]', data.weekly, {reset:true} );
+			switch (data.day) {
+				case "Monday" :
+					driver.click('input#cb_monbox');
+				case "Tuesday" :
+					driver.click('input#cb_tuebox');
+				case "Wednesday" :
+					driver.click('input#cb_wedbox');
+				case "Thursday" :
+					driver.click('input#cb_thubox');
+				case "Friday" :
+					driver.click('input#cb_fribox');
+				case "Saturday" :
+					driver.click('input#cb_satbox');
+				case "Sunday" :
+					driver.click('input#cb_sunbox');
+			};
+	
+		break;
+		case "Monthly" :
+			driver.click('input#rb_event_3');
+			driver.sendKeys('input[name="weeklybox"]', data.weekly, {reset:true} );
+
+
+		break;
+		case "Yearly" :
+			driver.click('input#');
+			driver.sendKeys('input[name="weeklybox"]', data.weekly, {reset:true} );
+
+
+
+
+
+
+
+
+	};
+	};*/
+	driver.wait(3000, function(){
+		this.click('button#post_event_buttton');	
+	});
+	return callback();
 
 
 
