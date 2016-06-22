b9a29f5bfd2a18642bf023512521ab895e9176a2
its4monika <monika@websitetoolbox.com>
2016-06-22 16:33:32 +0530

    Added startup script

--- forumAutomation/bin/startup.sh
+++ forumAutomation/bin/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 FOREVER='/usr/local/bin/forever'
-CHAT_LOG='/var/log/automation/forum.log'
+AUTOMATION_LOG='/var/log/automation/forum.log'
 OUT_LOG='/var/log/automation/out.log'
 ERR_LOG='/var/log/automation/err.log'
+AUTOMATION_HOME='/home/monika/project/git/QA-automation/forumAutomation'
+cd $AUTOMATION_HOME
+sudo "$FOREVER" start --minUptime 1000 --spinSleepTime 1000 -a -l "$AUTOMATION_LOG" -o "$OUT_LOG" -e "$ERR_LOG" "$AUTOMATION_HOME"/main.js
 
