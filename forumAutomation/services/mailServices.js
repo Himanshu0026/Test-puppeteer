5b71aa78d7ca0f3386f756b21f4b62d1f1c8c42a
its4monika <monika@websitetoolbox.com>
2016-07-04 17:41:38 +0530

    Modified variable name for attachments

--- forumAutomation/services/mailServices.js
+++ forumAutomation/services/mailServices.js
@@ -21,7 +21,7 @@ mailServices.sendMail = function(emailDetails, callback){
 		to: emailDetails.committerEmail, // list of receivers 
 		subject: emailDetails.commitId, // Subject line 
 		text: 'Hello ' + emailDetails.committerName+",\n\n Automation test result: "+emailDetails.testResult+"\n\nPlease find details of the automation test result attached herewith. " , // plaintext body 
-		attachments: emailDetails.attachment 
+		attachments: emailDetails.attachments 
 	};
 	console.log("mailOptions : "+JSON.stringify(mailOptions));
 	transporter.sendMail(mailOptions, function(error, info){
