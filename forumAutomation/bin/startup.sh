fd0eff45d77b424a726516f3a2521b38fd08c95d
its4monika <monika@websitetoolbox.com>
2016-07-18 12:49:15 +0530

    Modified log file path

--- forumAutomation/bin/startup.sh
+++ forumAutomation/bin/startup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 FOREVER='/usr/local/bin/forever'
-AUTOMATION_LOG='/var/log/automation/forum.log'
-OUT_LOG='/var/log/automation/out.log'
-ERR_LOG='/var/log/automation/err.log'
+AUTOMATION_LOG='/var/log/scripts/automation.log'
 AUTOMATION_HOME='/home/monika/project/git/QA-automation/forumAutomation'
 cd $AUTOMATION_HOME
-sudo "$FOREVER" start --minUptime 1000 --spinSleepTime 1000 -a -l "$AUTOMATION_LOG" -o "$OUT_LOG" -e "$ERR_LOG" "$AUTOMATION_HOME"/main.js
+sudo "$FOREVER" start --minUptime 1000 --spinSleepTime 1000 -a -l "$AUTOMATION_LOG" "$AUTOMATION_HOME"/main.js
 
