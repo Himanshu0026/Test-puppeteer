98656652f69c489dcbbe83b6fe65b4bb82545e0f
its4monika <monika@websitetoolbox.com>
2016-07-20 18:24:53 +0530

    Added check for fail cases before sending mails

--- forumAutomation/services/executorServices.js
+++ forumAutomation/services/executorServices.js
@@ -15,35 +15,52 @@ executorServices.execute = function(script){
 //It executes job. Take job details as argument, executed the job and initiates mail sending.
 executorServices.executeJob = function(commitDetails, callback){
 	//Executing gitdeploy.sh to update Forum's code for given branch name
-//if(commitDetails.branchName == "automation"){
+if(commitDetails.branchName == "automation"){
 	console.log("Executing gitdeploy.sh");
 	var gitDeployResult = executorServices.execute("sudo bash -c '/home/monika/gitdeploy.sh "+commitDetails.branchName+ "'");
 	//Executing automation test script
 	var testResult = executorServices.execute("sudo bash -c '/home/monika/project/git/QA-automation/forumAutomation/bin/automation.sh'");
-	//Adding test result with commit details
-	commitDetails['testResult'] = testResult;
-	//Addling log files as attachments
-	commitDetails['attachments'] = [
-                {   
-            		path: '/home/monika/project/git/QA-automation/forumAutomation/log/automation.log'
-        	},
-		{   
-            		path: '/home/monika/project/git/QA-automation/forumAutomation/log/fail.log'
-        	}
-	];
-	//initiating mail sending to committer
-	mailServices.sendMail(commitDetails, function(err){
-		if(err)
-			console.error("error occurred while sending email: "+err);
-		else
-			console.log("Mail sent successfully.");
-		//Deleting commit specific log files
-		fs.unlinkSync('/home/monika/project/git/QA-automation/forumAutomation/log/automation.log');
-		fs.unlinkSync('/home/monika/project/git/QA-automation/forumAutomation/log/fail.log');
-		console.log("Commit specific log files deleted.");
-		return callback();
+	var automationLogFile = '/home/monika/project/git/QA-automation/forumAutomation/log/automation.log';
+	var failLogFile = '/home/monika/project/git/QA-automation/forumAutomation/log/fail.log';
+	fs.stat(failLogFile, function(err, fileStat) {
+		if (err) {
+			if (err.code == 'ENOENT') {
+				console.log('fail.log does not exist.');
+			}
+		} else {
+			if (fileStat) {
+				var statJSON = util.inspect(fileStat);
+				if(statJSON.size != 0){
+					//Adding test result with commit details
+					commitDetails['testResult'] = testResult;
+					//Addling log files as attachments
+					commitDetails['attachments'] = [
+						{   
+					    		path: automationLogFile
+						},
+						{   
+					    		path: failLogFile
+						}
+					];
+					//initiating mail sending to committer
+					mailServices.sendMail(commitDetails, function(err){
+						if(err)
+							console.error("error occurred while sending email: "+err);
+						else
+							console.log("Mail sent successfully.");
+						//Deleting commit specific log files
+						fs.unlinkSync(automationLogFile);
+						fs.unlinkSync(failLogFile);
+						console.log("Commit specific log files deleted.");
+						return callback();
+					});
+				}
+			}
+		}
 	});
-/*}else{
+
+
+}else{
 	return callback();
-}*/
+}
 };
