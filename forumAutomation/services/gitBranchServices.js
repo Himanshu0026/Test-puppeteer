1be902128260100ee1c854dd463ad3e82394c9b2
its4monika <monika@websitetoolbox.com>
2016-08-03 17:01:03 +0530

    Fixed undefined redis client issue

--- forumAutomation/services/gitBranchServices.js
+++ forumAutomation/services/gitBranchServices.js
@@ -2,7 +2,7 @@
 var redisClient;
 var gitBranchServices = module.exports = {};
 
-gitBranchServices.deleteMatureCommitBranch = function(redisClient, callback){
+gitBranchServices.deleteMatureCommitBranch = function(){
 	redisClient.keys('pendingCommit*', function(err, pendingCommits){
 		if(pendingCommits && pendingCommits.length>0){
 			pendingCommits.forEach(function ( pc, index){
