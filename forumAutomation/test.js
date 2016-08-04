8f6365565b78fc154af76087248c47c4fb32b5bd
its4monika <monika@websitetoolbox.com>
2016-08-04 13:00:52 +0530

    test

---
+++ forumAutomation/test.js
@@ -0,0 +1,8 @@
+var execSync = require('child_process').execSync;
+execSync("touch 1.txt");
+execSync("touch 2.txt");
+execSync("touch 3.txt");
+execSync("touch 1.txt");
+execSync("touch 1.txt");
+execSync("touch 1.txt");
+execSync("touch 1.txt");
