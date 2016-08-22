32306ca71442908f977c826fc8892151e5939733
its4monika <monika@websitetoolbox.com>
2016-08-22 16:20:51 +0530

    Modified to run startup script by automation user

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-FOREVER='/usr/local/bin/forever'
-AUTOMATION_LOG='/var/log/scripts/automation.log'
-AUTOMATION_HOME='/etc/automation/'
-cp $AUTOMATION_HOME/bin/gitdeploy.sh /home/automation/
-cd $AUTOMATION_HOME
-"$FOREVER" start --minUptime 1000 --spinSleepTime 1000 -a -l "$AUTOMATION_LOG" "$AUTOMATION_HOME"/forumAutomation/main.js
+sudo -H -u automation bash -c '/etc/automation/bin/startup.sh'
 
