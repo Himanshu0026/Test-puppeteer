76e1d66278b3512e54c9ec03310bd3a0f285523e
its4monika <monika@websitetoolbox.com>
2016-08-26 08:35:05 +0530

    Added steps for testing topic, in-context login, forgot password

--- bin/automation.sh
+++ bin/automation.sh
@@ -4,14 +4,26 @@ cd $AUTOMATION_HOME
 echo "Tests executing for LOGIN" > "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=login >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
+echo "Tests executing for IN-CONTEXT LOGIN" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./forum/automation.js --feature=incontextlogin >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
 echo "Tests executing for REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
+echo "Tests executing for IN-CONTEXT REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./forum/automation.js --feature=inContextRegistration >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
+echo "Tests executing for BACKEND REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./forum/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
+echo "Tests executing for FORGOT PASSWORD" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./forum/automation.js --feature=forgotpassword >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
 echo "Tests executing for HIDE CATEGORY" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=hidecategory >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for BACKEND REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./forum/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
+echo "Tests executing for TOPIC RELATED FUNCTIONALITIES" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./forum/automation.js --feature=topic >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
 cat "$AUTOMATION_HOME"/log/automation.txt | grep FAIL > "$AUTOMATION_HOME"/log/fail.txt 
 cat "$AUTOMATION_HOME"/log/automation.txt | grep -i "tests execut"
--- forum/automation.js
+++ forum/automation.js
@@ -13,148 +13,28 @@ if(feature){
 }
 
 switch (feature) {
-    case "login":
-        casper.test.begin('Verify login functionality from home page with all valid and invalid scenarios ', function(test) {
-		var forumLogin = require("./testsuite/forum_login.js");
-		forumLogin.featureTest(casper, casper.test);
-		casper.run(function(){
-			test.done();
-		});
-	});
-        break;
-    case "editProfile":
-	casper.test.begin("Start 'Edit Profile' functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-		var editProfile = require("./testsuite/editprofile.js");
-		editProfile.featureTest(casper, casper.test);
-		casper.run(function() {
-			test.done();
-			test.assert(true);
-		});
-	});
-
-    case "editProfileWithSettings":
-	casper.test.begin("Start 'Edit Profile With Setting' functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-		var editProfile = require("./testsuite/editprofile.js");
-		editProfile.customFieldsTest(casper, casper.test);
-		casper.run(function() {
-			test.done();
-			test.assert(true);
-		});
-	});
-        
-    case "deleteAccount":
-	casper.test.begin("Start 'Delete Account' functionality from home page & verify content with all scenarios", function(test) {
-
-
-var feature = casper.cli.get('feature');
-if(feature){
-	casper.echo("Started testing for the feature: " + feature +"\n");
-}else{
-	casper.echo("It seems, you have not given any option.");
-}
-
-switch (feature) {
-    /*case "login":
-        casper.test.begin('Verify login functionality from home page with all valid and invalid scenarios ', function(test) {
-		var forumLogin = require("./testsuite/forum_login.js");
-		forumLogin.featureTest(casper, casper.test);
-
-		var deleteAccount = require("./testsuite/deleteAccount.js");
-		var x = require('casper').selectXPath;
-		deleteAccount.featureTest(casper, casper.test, x);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-		
-    case "deleteAccountWithSettings":
-	casper.test.begin("Start 'Delete Account With Setting' functionality from home page & verify content with all scenarios", function(test) {
-
-		var deleteAccount = require("./testsuite/deleteAccount.js");
-		deleteAccount.customFieldsTest(casper, casper.test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        
-        break;
-
-    case "generalPermission":
-	casper.test.begin("Start 'General Permission' functionality from home page & verify content with all scenarios", function(test) {
-
-		var generalPermission = require("./testsuite/generalPermission.js");
-		var x = require('casper').selectXPath;
-		generalPermission.featureTest(casper, casper.test, x);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        
-        break;
-    case "register":
-    	casper.test.begin('REGISTRATION TEST', function(test) {
-		var forumRegister = require("./testsuite/register.js");
-		forumRegister.featureTest(casper, test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        break;
-	case "registerWithSettings":
-    	casper.test.begin('REGISTRATION WITH SETTINGS TEST', function(test) {
-		var forumRegister = require("./testsuite/register.js");
-		forumRegister.registerWithSettings(casper, test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        break;
-        case "inContextRegistration":
-    	casper.test.begin('IN CONTEXT REGISTRATION TEST', function(test) {
-		var inContextForumRegister = require("./testsuite/inContextRegister.js");
-		inContextForumRegister.featureTest(casper, test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        break;
-         case "backEndRegistration":
-    	casper.test.begin('BACK END REGISTRATION TEST', function(test) {
-		var backEndRegister = require("./testsuite/backEndRegister.js");
-		backEndRegister.featureTest(casper, test);
-
-		casper.run(function(){
-			test.done();
-		});
-	});
-	casper.wait(5000, function(){
-		casper.test.begin('Verify inContext Login functionlity ', function(test) {
-			 var inContextLogin = require("./testsuite/inContextLogin.js");
-			 inContextLogin.featureTest(casper, casper.test);
-			 casper.run(function(){
+    	case "login":
+		casper.test.begin('Verify login functionality from home page with all valid and invalid scenarios ', function(test) {
+			var forumLogin = require("./testsuite/forum_login.js");
+			forumLogin.featureTest(casper, casper.test);
+			casper.run(function(){
 				test.done();
 			});
 		});
-	});
-        break;*/
-    case "profile":
-	/*casper.test.begin("Start 'Edit Profile' functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-		var editProfile = require("./testsuite/editprofile.js");
-		editProfile.featureTest(casper, casper.test);
-		casper.run(function() {
-			test.done();
-			test.assert(true);
+		break;
+
+    	case "editProfile":
+		casper.test.begin("Start 'Edit Profile' functionality from home page & verify content with all valid and invalid scenarios", function(test) {
+			var editProfile = require("./testsuite/editprofile.js");
+			editProfile.featureTest(casper, casper.test);
+			casper.run(function() {
+				test.done();
+				test.assert(true);
+			});
 		});
-	});
+		break;
 
-
-    	casper.wait(5000, function(){
+    	case "editProfileWithSettings":
 		casper.test.begin("Start 'Edit Profile With Setting' functionality from home page & verify content with all valid and invalid scenarios", function(test) {
 			var editProfile = require("./testsuite/editprofile.js");
 			editProfile.customFieldsTest(casper, casper.test);
@@ -163,9 +43,9 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-        });
-   
-	casper.wait(5000, function(){
+		break;
+        
+    	case "deleteAccount":
 		casper.test.begin("Start 'Delete Account' functionality from home page & verify content with all scenarios", function(test) {
 
 			var deleteAccount = require("./testsuite/deleteAccount.js");
@@ -176,9 +56,9 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-	});	
-    
-	casper.wait(5000, function(){
+		break;
+		
+    	case "deleteAccountWithSettings":
 		casper.test.begin("Start 'Delete Account With Setting' functionality from home page & verify content with all scenarios", function(test) {
 
 			var deleteAccount = require("./testsuite/deleteAccount.js");
@@ -188,33 +68,34 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-        });*/
-	//casper.start();
+		
+		break;
 
-	//casper.wait(5000, function(){
-		casper.test.begin("Start 'Forum Listing Page' functionality from home page & verify content with all scenarios", function(test) {
+    	case "generalPermission":
+		casper.test.begin("Start 'General Permission' functionality from home page & verify content with all scenarios", function(test) {
 
-			var forumListingPage = require("./testsuite/forumListingPage.js");
-			forumListingPage.featureTest(casper, casper.test);
+			var generalPermission = require("./testsuite/generalPermission.js");
+			var x = require('casper').selectXPath;
+			generalPermission.featureTest(casper, casper.test, x);
 			casper.run(function(){
 				test.done();
 				test.assert(true);
 			});
 		});
-       // });
-        break;
+		break;
 
-    case "register":
-    	casper.test.begin('REGISTRATION TEST', function(test) {
-		var forumRegister = require("./testsuite/register.js");
-		forumRegister.featureTest(casper, test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
+    	case "register":
+	    	casper.test.begin('REGISTRATION TEST', function(test) {
+			var forumRegister = require("./testsuite/register.js");
+			forumRegister.featureTest(casper, test);
+			casper.run(function(){
+				test.done();
+				test.assert(true);
+			});
 		});
-	});
-        
-	casper.wait(5000, function(){
+		break;
+
+	case "registerWithSettings":
 	    	casper.test.begin('REGISTRATION WITH SETTINGS TEST', function(test) {
 			var forumRegister = require("./testsuite/register.js");
 			forumRegister.registerWithSettings(casper, test);
@@ -223,9 +104,9 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-	});
-        
-	casper.wait(5000, function(){
+		break;
+
+        case "inContextRegistration":
 	    	casper.test.begin('IN CONTEXT REGISTRATION TEST', function(test) {
 			var inContextForumRegister = require("./testsuite/inContextRegister.js");
 			inContextForumRegister.featureTest(casper, test);
@@ -234,9 +115,9 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-	});
-        
-	casper.wait(5000, function(){
+		break;
+
+        case "backEndRegistration":
 	    	casper.test.begin('BACK END REGISTRATION TEST', function(test) {
 			var backEndRegister = require("./testsuite/backEndRegister.js");
 			backEndRegister.featureTest(casper, test);
@@ -245,8 +126,8 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-	});
-        break;
+		break;
+
         case "verifyCategoryPermissions":
 	    	casper.test.begin('VERIFY CATEGORY PERMISSIONS TEST', function(test) {
 			var verifyCategoryPermissions = require("./testsuite/verifyCategoryPermissions.js");
@@ -256,209 +137,84 @@ switch (feature) {
 				test.assert(true);
 			});
 		});
-        break;
-        
+		break;
+
+        case "uploadAttachment":
+	    	casper.test.begin('VERIFY UPLOAD ATTACHMENT TEST', function(test) {
+			var uploadAttachment = require("./testsuite/uploadAttachment.js");
+			uploadAttachment.featureTest(casper, test);
+			casper.run(function(){
+				test.done();
+				test.assert(true);
+			});
+		});
+		break;
+
 	case "topic":
 		casper.test.begin("Start New Topic functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-
 			var newTopic = require("./testsuite/newTopic.js");
 			var x = require('casper').selectXPath;
 			newTopic.featureTest(casper, casper.test, x);
-
-        break;
-        case "verifyCategoryPermissions":
-    	casper.test.begin('VERIFY CATEGORY PERMISSIONS TEST', function(test) {
-		var verifyCategoryPermissions = require("./testsuite/verifyCategoryPermissions.js");
-		verifyCategoryPermissions.featureTest(casper, test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        break;
-        case "uploadAttachment":
-    	casper.test.begin('VERIFY UPLOAD ATTACHMENT TEST', function(test) {
-		var uploadAttachment = require("./testsuite/uploadAttachment.js");
-		uploadAttachment.featureTest(casper, test);
-		casper.run(function(){
-			test.done();
-			test.assert(true);
-		});
-	});
-        break;
-	case "topic":
-	casper.test.begin("Start New Topic functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-
-		var newTopic = require("./testsuite/newTopic.js");
-		var x = require('casper').selectXPath;
-		newTopic.featureTest(casper, casper.test, x);
-
-		
 			casper.run(function(){
 				test.done();
 			});
 		});
-        
-        break;
-   
-	case "forgotpassword":
-        casper.test.begin('Verify forgot your password functionality from home page ', function(test) {
-		var forumLogin = require("./testsuite/forgotPassword.js");
-		forumLogin.featureTest(casper, casper.test);
-		casper.run(function(){
-			test.done();
+		break;
+
+    	case "forgotpassword":
+		casper.test.begin('Verify forgot your password functionality from home page ', function(test) {
+			var forgotpwd = require("./testsuite/forgotPassword.js");
+			forgotpwd.featureTest(casper, casper.test);
+			casper.run(function(){
+				test.done();
+			});
 		});
-	});
+		break;
 
-        break;
-
-	
-        
-
-        
-        break;
-    case "postreply":
-        casper.test.begin("Start reply topic functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-
-		var postAReply = require("./testsuite/postAReply.js");
-		var x = require('casper').selectXPath;
-		postAReply.postAReplyFeature(casper, casper.test, x);
-		casper.run(function(){
-			test.done();
-		});
-	});
-        break;
- case "edittopic":
-        casper.test.begin("Start edit Topic functionality from home page & verify content with all valid and invalid scenarios", function(test) {
-
-		var editTopic = require("./testsuite/editTopic.js");
-		var x = require('casper').selectXPath;
-		editTopic.editTopicFeature(casper, casper.test, x);
-		casper.run(function(){
-			test.done();
-		});
-	});
-        break;
-	case "forgotpassword":
-        casper.test.begin('Verify forgot your password functionality from home page ', function(test) {
-		var forumLogin = require("./testsuite/forgotPassword.js");
-		forumLogin.featureTest(casper, casper.test);
-		casper.run(function(){
-			test.done();
-		});
-	});
-        break;
-case "deletetopic":
-        casper.test.begin("Delete Topic functionality from home page & verify deleted post", function(test) {
-
-		var deleteTopic = require("./testsuite/deleteTopic.js");
-		var x = require('casper').selectXPath;
-		deleteTopic.featureTest(casper, casper.test, x);		
-		casper.run(function(){
-			test.done();
-		});
-	});
-        break;
 	case "calendar" :
 		 casper.test.begin('Verify calendar functionlity ', function(test) {
-		var x = require('casper').selectXPath;
-		 var calendar = require("./testsuite/calendar.js");
-		 calendar.featureTest(casper, casper.test,x);
-		 casper.run(function(){
-			test.done();
+			 var x = require('casper').selectXPath;
+			 var calendar = require("./testsuite/calendar.js");
+			 calendar.featureTest(casper, casper.test,x);
+			 casper.run(function(){
+				test.done();
+			});
 		});
-	});
-        break;
+		break;
+
 	case "incontextlogin" :
 		 casper.test.begin('Verify inContext Login functionlity ', function(test) {
-		 var inContextLogin = require("./testsuite/inContextLogin.js");
-		 inContextLogin.featureTest(casper, casper.test);
-		 casper.run(function(){
-			test.done();
+			 var inContextLogin = require("./testsuite/inContextLogin.js");
+			 inContextLogin.featureTest(casper, casper.test);
+			 casper.run(function(){
+				test.done();
+			});
 		});
-	});
-        break;
-	case "pintopic" :
-		 casper.test.begin('Verify pin topic functionlity ', function(test) {
-		 var pinTopic = require("./testsuite/pinTopic.js");
-		 var x = require('casper').selectXPath;
-		 pinTopic.pinUnPinFeature(casper, casper.test, x);
-		 casper.run(function(){
-			test.done();
-		});
-	});
-        break;
-	case "lockunlock" :
-		 casper.test.begin('Verify lock-unLock topic functionlity ', function(test) {
-		 var lock_unLockTopic = require("./testsuite/lock_unLockTopic.js");
-		 var x = require('casper').selectXPath;
-		 lock_unLockTopic.lockUnLockFeature(casper, casper.test, x);
-		 casper.run(function(){
-			test.done();
-		});
-	});
-        break;
-	case "poll" :
-		 casper.test.begin('Verify poll topic functionlity ', function(test) {
-		 var poll = require("./testsuite/poll.js");
-		 var x = require('casper').selectXPath;
-		 poll.pollFeature(casper, casper.test, x);
-		 casper.run(function(){
-			test.done();
-		});
-	});
-        break;
-	case "movetopic" :
-		 casper.test.begin('Verify move topic functionlity ', function(test) {
-		 var moveTopic = require("./testsuite/moveTopic.js");
-		 var x = require('casper').selectXPath;
-		 moveTopic.moveTopicFeature(casper, casper.test, x);
-		 casper.run(function(){
-			test.done();
-		});
-	});
-        break;
+		break;
 
 	case "hidecategory" :
 		 casper.test.begin('Verify hide/un-hide category functionlity ', function(test) {
-		 var hideCategory = require("./testsuite/hideCategory.js");
-		 var x = require('casper').selectXPath;
-		 hideCategory.hideCategoryFeature(casper, casper.test, x);
-		 casper.run(function(){
-			test.done();
+			 var hideCategory = require("./testsuite/hideCategory.js");
+			 var x = require('casper').selectXPath;
+			 hideCategory.hideCategoryFeature(casper, casper.test, x);
+			 casper.run(function(){
+				test.done();
+			});
 		});
-	});
-        break;
-
-
+		break;
 
 	case "thumpsUpDown" :
 		 casper.test.begin('Verify thumps up/down functionlity ', function(test) {
-		 var thumpsUpDown = require("./testsuite/thumpsUpDown.js");
-		 thumpsUpDown.featureTest(casper, casper.test);
-		 casper.run(function(){
-			test.done();
+			 var thumpsUpDown = require("./testsuite/thumpsUpDown.js");
+			 thumpsUpDown.featureTest(casper, casper.test);
+			 casper.run(function(){
+				test.done();
+			});
 		});
-	});
-        break;
-    default:
-	casper.echo("Please select any feature from options given below. For ex: casperjs main.js <option>.\n"); 
-        casper.echo("Options:");
-	casper.echo("	register");
-	casper.echo("	registerWithSettings");
-	casper.echo("	inContextRegistration");
-	casper.echo("	backEndRegistration");
-	casper.echo("	verifyCategoryPermissions");
-	casper.echo("	login");
-	casper.echo("	topic");
-	casper.echo("	postreply\n");
-	casper.echo("	edittopic\n");
-	casper.echo("	deletetopic\n");
-	casper.echo("	calendar\n");
-	casper.echo("	poll\n");
-	casper.echo("Relevant test data has to be fed in JSON format in files placed for each feature in '<current directory>/testData/'.");
-	casper.exit();
-
+		break;
+	
+	default:
+		casper.exit();
 };
 
 
