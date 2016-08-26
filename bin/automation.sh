d5c34f79582f68bcc9c30271415bcc83fcb15bfa
its4monika <monika@websitetoolbox.com>
2016-08-26 10:49:18 +0530

    Modified for printing format in automation.sh

--- bin/automation.sh
+++ bin/automation.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 AUTOMATION_HOME='/etc/automation'
 cd $AUTOMATION_HOME
-echo "Tests executing for LOGIN" > "$AUTOMATION_HOME"/log/automation.txt
+printf "Tests executing for LOGIN\n" > "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=login >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for IN-CONTEXT LOGIN" >> "$AUTOMATION_HOME"/log/automation.txt
+printf "\nTests executing for IN-CONTEXT LOGIN\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=incontextlogin >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
+printf "\nTests executing for REGISTRATION\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for IN-CONTEXT REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
+printf "\nTests executing for IN-CONTEXT REGISTRATION\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=inContextRegistration >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for BACKEND REGISTRATION" >> "$AUTOMATION_HOME"/log/automation.txt
+printf "\nTests executing for BACKEND REGISTRATION\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for FORGOT PASSWORD" >> "$AUTOMATION_HOME"/log/automation.txt
+printf "\nTests executing for FORGOT PASSWORD\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=forgotpassword >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for HIDE CATEGORY" >> "$AUTOMATION_HOME"/log/automation.txt
+echo "\nTests executing for HIDE CATEGORY\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=hidecategory >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-echo "Tests executing for TOPIC RELATED FUNCTIONALITIES" >> "$AUTOMATION_HOME"/log/automation.txt
+echo "\nTests executing for TOPIC RELATED FUNCTIONALITIES\n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./forum/automation.js --feature=topic >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
 cat "$AUTOMATION_HOME"/log/automation.txt | grep FAIL > "$AUTOMATION_HOME"/log/fail.txt 
