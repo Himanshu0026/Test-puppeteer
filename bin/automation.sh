4a2503ac2ba5201d44c700c7611f92d011d1ab79
shipragoel123 <shipra@websitetoolbox.com>
2016-10-06 17:44:24 +0530

    Add "ForumListingPage" and "forumListingPageForSubCategory" features.

--- bin/automation.sh
+++ bin/automation.sh
@@ -25,12 +25,12 @@ sleep 1
 printf "\nTests executing for EDIT PROFILE: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./automationScripts/automation.js --feature=editProfile >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-#printf "\nTests executing for Forum Listing Page: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-#casperjs test ./automationScripts/automation.js --feature=forumListingPage >> "$AUTOMATION_HOME"/log/automation.txt
-#sleep 1
-#printf "\nTests executing for Forum Listing Page For Sub Category: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-#casperjs test ./automationScripts/automation.js --feature=forumListingPageForSubCategory >> "$AUTOMATION_HOME"/log/automation.txt
-#sleep 1
+printf "\nTests executing for Forum Listing Page: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./automationScripts/automation.js --feature=forumListingPage >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
+printf "\nTests executing for Forum Listing Page For Sub Category: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./automationScripts/automation.js --feature=forumListingPageForSubCategory >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
 #printf "\nTests executing for EDIT PROFILE WITH SETTINGS: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./forum/automation.js --feature=editProfileWithSettings >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
