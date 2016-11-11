f820c053fe4c8046430fae4e2d369cb338f22b92
shipragoel123 <shipra@websitetoolbox.com>
2016-11-11 10:38:01 +0530

    Add changes in automation.sh

--- bin/automation.sh
+++ bin/automation.sh
@@ -4,27 +4,27 @@ cd $AUTOMATION_HOME
 printf "Tests executing for LOGIN: \n" > "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./automationScripts/automation.js --feature=login >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=incontextlogin >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
-printf "\nTests executing for REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
+#printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=incontextlogin >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
+#printf "\nTests executing for REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
 #printf "\nTests executing for IN-CONTEXT REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./forum/automation.js --feature=inContextRegistration >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
-printf "\nTests executing for BACKEND REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
-printf "\nTests executing for FORGOT PASSWORD: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=forgotpassword >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
-printf "\nTests executing for HIDE CATEGORY: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=hidecategory >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
-printf "\nTests executing for EDIT PROFILE: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=editProfile >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
+#printf "\nTests executing for BACKEND REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
+#printf "\nTests executing for FORGOT PASSWORD: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=forgotpassword >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
+#printf "\nTests executing for HIDE CATEGORY: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=hidecategory >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
+#printf "\nTests executing for EDIT PROFILE: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=editProfile >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
 #printf "\nTests executing for EDIT PROFILE WITH SETTINGS: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./forum/automation.js --feature=editProfileWithSettings >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
@@ -51,12 +51,12 @@ sleep 1
 #printf "\nTests executing for Verify move topic functionlity : \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./automationScripts/automation.js --feature=movetopic >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
-printf "\nTests executing for Forum Listing Page: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=forumListingPage >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
-printf "\nTests executing for Custom Profile Fields: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=customProfileField >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
+#printf "\nTests executing for Forum Listing Page: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=forumListingPage >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
+#printf "\nTests executing for Custom Profile Fields: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=customProfileField >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
 #printf "\nTests executing for Forum Listing Page For Sub Category: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./automationScripts/automation.js --feature=forumListingPageForSubCategory >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
