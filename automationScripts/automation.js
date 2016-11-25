13dabd38f82ae9b448c28059e4e060d500183647
hani1991 <hani@websitetoolbox.com>
2016-11-25 13:43:31 +0530

    Incontext LoginfrmStartTopic IncontextLoginfrmLikeTopicspage

--- automationScripts/automation.js
+++ automationScripts/automation.js
@@ -20,14 +20,25 @@ switch (feature) {
 			forumLogin.featureTest(casper, casper.test);
 			casper.run(function(){
 				test.done();
-			})
+			});
+		});
+	break;
+	
+	case "inContextLogin":
+		casper.test.begin('Verify inContext Login functionlity',function(test){
+			var inContextLogin=require("./testsuite/main/inContextLogin.js");
+			inContextLogin.featureTest(casper,casper.test);
+			casper.run(function(){
+				test.done();
+			});
 		});
 	break;
 	
 	default:
 		casper.echo("Please select any feature from options given below. For ex: casperjs automation.js <option>.\n"); 
         	casper.echo("Options:");
-		casper.echo("	login");
+		casper.echo("login");
+		casper.echo("inContextLogin");
 		casper.exit();
 };
 
--- automationScripts/testsuite/wait.js
+++ automationScripts/testsuite/wait.js
@@ -3,10 +3,10 @@ var wait = module.exports = {};
 
 wait.waitForElement = function(element, driver, callback){
 	driver.waitForSelector(element, function success() {
-		casper.echo('Form Selector ' +element+ ' Found', 'INFO');
+		driver.echo('Form Selector ' +element+ ' Found', 'INFO');
 		return callback(null, true);
 	}, function fail() {
-		casper.echo('Form Selector ' +element+ ' Not Found', 'ERROR');
+		driver.echo('Form Selector ' +element+ ' Not Found', 'ERROR');
 		return(null, false);
 	});
 };
