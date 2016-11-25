c69be068eebcc3045b2e9e9a079e5c3be56209b6
rajan41 <rajan@websitetoolbox.com>
2016-11-25 16:03:48 +0530

    add back enable feature in register.js

--- automationScripts/testsuite/cases/register.js
+++ automationScripts/testsuite/cases/register.js
@@ -1,6 +1,7 @@
 'use strict';
 var json = require('../../testdata/registerData.json');
 var config = require('../../../config/config.json');
+var utils = require('../utils.js');
 var registerMethod = require('../methods/register.js');
 var wait = require('../wait.js');
 var registerTests=module.exports = {};
@@ -11,43 +12,131 @@ var screenShotsDir = config.screenShotsLocation + 'register/';
 //Getting 'User Accounts' Field Valu If, Enabled, Then Filling Data For Testing
 registerTests.userAccountsEnable  = function() {
 
-	casper.echo('******************** case-1 ************************');
-	casper.echo('Getting User Accounts Field Valu If, Enabled, Then Filling Data For Testing');
-	casper.echo('********************************************');
-	//Login To Forum BackEnd 
-	registerMethod.loginToForumBackEnd(casper, casper.test, function() {
-		casper.echo('Successfully Login To Forum Back End...........', 'INFO');
-		//Clicking On "General" Tab Under Settings 
-		wait.waitForElement('div#my_account_forum_menu a[data-tooltip-elm="ddSettings"]', casper, function(err, isExist) {
-			if(!err){
-				if(isExist) {
-					casper.test.assertExists('div#my_account_forum_menu a[data-tooltip-elm="ddSettings"]');
-					casper.click('div#my_account_forum_menu a[data-tooltip-elm="ddSettings"]');
-					casper.test.assertExists('div#ddSettings a[href="/tool/members/mb/settings?tab=General"]');
-					casper.click('div#ddSettings a[href="/tool/members/mb/settings?tab=General"]');
-					casper.echo('Successfully open forum settings form.....', 'INFO');
-					//Getting 'User Accounts' Field Value, If, Enabled, Then Filling Data For Testing
-					wait.waitForElement('#REQreg', casper, function(err, isExist) {
-						if(!err){
-							if(isExist) {
-								utils.enableorDisableCheckbox('REQreg', true, casper, function(err) {
-									if (!err){
-										casper.echo("User Accounts Checkbox Has Been Enabled For Registered User", 'INFO');
-										casper.test.assertExists('.button.btn-m.btn-blue');
-										casper.click('.button.btn-m.btn-blue');
-									}	
-								});	
-							} else {
-								casper.echo('User Accounts Checkbox Not Found', 'ERROR');
-							}	
+	casper.echo('******************** case-1 ************************', 'INFO');
+	casper.echo('Getting User Accounts Field Valu If, Enabled, Then Filling Data For Testing', 'INFO');
+	casper.echo('********************************************', 'INFO');
+	 
+	 //Open Back-End URL And Get Title
+	casper.thenOpen(config.backEndUrl, function() {
+		casper.echo('Title of the page :' +this.getTitle(), 'INFO');
+		try {
+			casper.test.assertExists('a[href="/tool/members/login?action=logout"]');
+			casper.click('a[href="/tool/members/login?action=logout"]');
+		}catch(e) {
+			casper.test.assertDoesntExist('a[href="/tool/members/login?action=logout"]');
+		}
+	});
+	//Login To Forum BackEnd
+	casper.then(function() {
+		registerMethod.loginToForumBackEnd(casper, casper.test, function(err) {
+			 if(!err){
+				casper.echo('Logged-in successfully from back-end', 'INFO');
+				wait.waitForElement('div#my_account_forum_menu', casper, function(err, isExist) {
+					if(!err){
+						if(isExist) {
+							casper.test.assertExists('div#my_account_forum_menu a[data-tooltip-elm="ddUsers"]');
+							casper.click('div#my_account_forum_menu a[data-tooltip-elm="ddUsers"]');
+							casper.test.assertExists('div#ddUsers a[href="/tool/members/mb/fields"]');
+							casper.click('div#ddUsers a[href="/tool/members/mb/fields"]');
+							wait.waitForElement('form[name="posts"]', casper, function(err, isExist) {
+								if(!err){
+									if(isExist) {
+										casper.click('form#custom_fields_table input');
+										casper.click('form#custom_fields_table button');
+										wait.waitForElement('p:nth-child(4)', casper, function(err, isExist) {
+											if(!err){
+												if(isExist) {
+													
+													var msg = casper.fetchText('p:nth-child(4)');
+													casper.echo('Success Message: '+msg, 'INFO');
+													casper.echo('all custom profile fields have been deleted', 'INFO');
+												}else{
+												casper.echo('Message is not generated Found', 'ERROR');
+												}
+											}
+										});
+									}else{
+										casper.echo('custom_fields Not Found', 'ERROR');
+									}
+								}
+							});
+							casper.then(function() {
+								casper.test.assertExists('div#my_account_forum_menu a[data-tooltip-elm="ddUsers"]');
+								casper.click('div#my_account_forum_menu a[data-tooltip-elm="ddUsers"]');
+								casper.test.assertExists('div#ddUsers a[href="/tool/members/mb/fields?action=default_registration_option"]');
+								casper.click('div#ddUsers a[href="/tool/members/mb/fields?action=default_registration_option"]');
+								wait.waitForElement('div.heading', casper, function(err, isExist) {
+									if(!err){
+										if(isExist) {
+											casper.fill('form[name="posts"]', {
+												'required_name' : 1,
+												'required_imType' : 1,
+												'required_dob' : 1,
+												'required_signature' : 1
+											}, true);
+											casper.click('form[name="posts"] button');
+											wait.waitForElement('font[color="red"]', casper, function(err, isExist) {
+												if(!err){
+													if(isExist) {
+														var successMsg = casper.fetchText('font[color="red"]');
+														casper.echo('success message : '+successMsg, 'INFO');
+														casper.echo('success message is verified', 'INFO');
+													}else{
+														 casper.echo('Message is not generated Found', 'ERROR');
+													}
+												}
+											});
+										}else{
+										     casper.echo('Default_registration_option Link Not Found', 'ERROR');
+										}
+									}
+								});				
+							});		
+						} else {
+							casper.echo('Account_orum_Menu Not Found', 'ERROR');
 						}
-					});
-				} else {
-					casper.echo('Setting Link Not Found', 'ERROR');
-				}
+					}
+			    });
 			}
 		});
-                
+	});
+	//Login To Forum BackEnd
+	casper.thenOpen(config.backEndUrl, function() {
+		registerMethod.loginToForumBackEnd(casper, casper.test, function(err) {
+			 if(!err){
+				casper.echo('Successfully Login To Forum Back End...........', 'INFO');
+				//Clicking On "General" Tab Under Settings 
+				wait.waitForElement('div#my_account_forum_menu a[data-tooltip-elm="ddSettings"]', casper, function(err, isExist) {
+					if(!err){
+						if(isExist) {
+							casper.test.assertExists('div#my_account_forum_menu a[data-tooltip-elm="ddSettings"]');
+							casper.click('div#my_account_forum_menu a[data-tooltip-elm="ddSettings"]');
+							casper.test.assertExists('div#ddSettings a[href="/tool/members/mb/settings?tab=General"]');
+							casper.click('div#ddSettings a[href="/tool/members/mb/settings?tab=General"]');
+							casper.echo('Successfully open forum settings form.....', 'INFO');
+							//Getting 'User Accounts' Field Value, If, Enabled, Then Filling Data For Testing
+							wait.waitForElement('#REQreg', casper, function(err, isExist) {
+								if(!err){
+									if(isExist) {
+										utils.enableorDisableCheckbox('REQreg', true, casper, function(err) {
+											if (!err){
+												casper.echo("User Accounts Checkbox Has Been Enabled For Registered User", 'INFO');
+												casper.test.assertExists('.button.btn-m.btn-blue');
+												casper.click('.button.btn-m.btn-blue');
+											}	
+										});	
+									} else {
+										casper.echo('User Accounts Checkbox Not Found', 'ERROR');
+									}	
+								}
+							});
+						} else {
+							casper.echo('Setting Link Not Found', 'ERROR');
+						}
+					}
+				});
+			}        
+		});
 	});
 	
 	casper.thenOpen(config.url, function() {
@@ -71,9 +160,9 @@ registerTests.userAccountsEnable  = function() {
 //test case for register to application by leaving blank username and verify error message
 registerTests.blankUsername = function() {
 	casper.then(function() {
-		casper.echo('******************** case-2 ************************');
-		casper.echo('test case for register to application by leaving blank username and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-2 ************************', 'INFO');
+		casper.echo('test case for register to application by leaving blank username and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['blankUsername'], casper, function(err){
 			if(!err) {
 				casper.echo('register by leaving blank username and verify error message', 'INFO');
@@ -99,8 +188,8 @@ registerTests.blankUsername = function() {
 //test case for register to application by leaving blank email and verify error message
 registerTests.blankEmail = function() {
 	casper.then(function() {
-		casper.echo('******************** case-3 ************************');
-		casper.echo('test case for register to application by leaving blank email and verify error message');
+		casper.echo('******************** case-3 ************************', 'INFO');
+		casper.echo('test case for register to application by leaving blank email and verify error message', 'INFO');
 		casper.echo('********************************************');
 		registerMethod.registerToApp(json['blankEmail'], casper, function(err){
 			if(!err) {
@@ -127,9 +216,9 @@ registerTests.blankEmail = function() {
 //test case for register to application by leaving blank password and verify error message
 registerTests.blankPassword = function() {
 	casper.then(function() {
-		casper.echo('******************** case-4 ************************');
-		casper.echo('test case for register to application by leaving blank password and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-4 ************************', 'INFO');
+		casper.echo('test case for register to application by leaving blank password and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['blankPassword'], casper, function(err){
 			if(!err) {
 				casper.echo('register by leaving blank password and verify error message', 'INFO');
@@ -155,9 +244,9 @@ registerTests.blankPassword = function() {
 //test case for register to application by existing username and verify error message
 registerTests.existUsername = function() {		
 	casper.then(function() {
-		casper.echo('******************** case-5 ************************');
-		casper.echo('test case for register to application by existing username and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-5 ************************', 'INFO');
+		casper.echo('test case for register to application by existing username and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['existUsername'], casper, function(err){
 			if(!err) {
 				casper.echo('register by existing username and verify error message', 'INFO');
@@ -200,9 +289,9 @@ registerTests.existUsername = function() {
 //test case for register to application by existing email and verify error message
 registerTests.existEmail = function() {
 	casper.then(function() {
-		 casper.echo('******************** case-6 ************************');
-		 casper.echo('test case for register to application by existing email and verify error message');
-		 casper.echo('********************************************');
+		 casper.echo('******************** case-6 ************************', 'INFO');
+		 casper.echo('test case for register to application by existing email and verify error message', 'INFO');
+		 casper.echo('********************************************', 'INFO');
 		 registerMethod.registerToApp(json['existEmail'], casper, function(err){
 			if(!err) {
 				casper.echo('register by existing email and verify error message', 'INFO');
@@ -246,9 +335,9 @@ registerTests.existEmail = function() {
 //test case for register to application by leaving blank im-id and verify error message
 registerTests.blankImId = function() {
 	casper.then(function() {
-		casper.echo('******************** case-7 ************************');
-		casper.echo('test case for register to application by leaving blank im-id and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-7 ************************', 'INFO');
+		casper.echo('test case for register to application by leaving blank im-id and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['blankImId'], casper, function(err){
 			if(!err) {
 				casper.echo('register by leaving blank im-id and verify error message', 'INFO');
@@ -292,9 +381,9 @@ registerTests.blankImId = function() {
 //test case for register to application by leaving blank birthday and verify error message
 registerTests.blankBirthday = function() {
 	casper.then(function() {
-		casper.echo('******************** case-8 ************************');
-		casper.echo('test case for register to application by leaving blank birthday and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-8 ************************', 'INFO');
+		casper.echo('test case for register to application by leaving blank birthday and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['blankBirthday'], casper, function(err){
 			if(!err) {
 				casper.echo('register by leaving blank birthday and verify error message', 'INFO');
@@ -338,9 +427,9 @@ registerTests.blankBirthday = function() {
 //test case for register to application by invalid email and verify error message
 registerTests.invalidEmail = function() {
 	casper.then(function() {
-		casper.echo('******************** case-9 ************************');
-		casper.echo('test case for register to application by invalid email and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-9 ************************', 'INFO');
+		casper.echo('test case for register to application by invalid email and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['invalidEmail'], casper, function(err){
 			if(!err) {
 				casper.echo('register by invalid email and verify error message', 'INFO');
@@ -383,9 +472,9 @@ registerTests.invalidEmail = function() {
 //test case for register to application by leaving blank signature and verify error message
 registerTests.blankSignature = function() {
 	casper.then(function() {
-		casper.echo('******************** case-10 ************************');
-		casper.echo('test case for register to application by leaving blank signature and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-10 ************************', 'INFO');
+		casper.echo('test case for register to application by leaving blank signature and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['blankSignature'], casper, function(err){
 			if(!err) {
 				casper.echo('register by leaving blank signature and verify error message', 'INFO');
@@ -401,9 +490,9 @@ registerTests.blankSignature = function() {
 //test case for register to application by existing username and email and verify error message
 registerTests.existUsernameEmail= function() {
 	casper.then(function() {
-		casper.echo('******************** case-11 ************************');
-		casper.echo('test case for register to application by existing username and email and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-11 ************************', 'INFO');
+		casper.echo('test case for register to application by existing username and email and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['existUsernameEmail'], casper, function(err){
 			if(!err) {
 				casper.echo('register by existing username and email and verify error message', 'INFO');
@@ -449,9 +538,9 @@ registerTests.existUsernameEmail= function() {
 //test case for register to application by valid data and verify error message
 registerTests.validInfo= function() {
 	casper.then(function() {
-		casper.echo('******************** case-12 ************************');
-		casper.echo('test case for register to application by valid data and verify error message');
-		casper.echo('********************************************');
+		casper.echo('******************** case-12 ************************', 'INFO');
+		casper.echo('test case for register to application by valid data and verify error message', 'INFO');
+		casper.echo('********************************************', 'INFO');
 		registerMethod.registerToApp(json['validInfo'], casper, function() {
 			casper.echo('Processing to registration on forum.....', 'INFO');
 			registerMethod.redirectToLogout(casper, casper.test, function() {});
