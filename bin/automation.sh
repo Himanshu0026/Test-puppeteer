bc35894195f2453bd9f4474ae3fd965afe65c083
shipragoel123 <shipra@websitetoolbox.com>
2016-11-28 18:22:47 +0530

    Changes.

--- bin/automation.sh
+++ bin/automation.sh
@@ -7,9 +7,9 @@ sleep 1
 #printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./automationScripts/automation.js --feature=incontextlogin >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
-printf "\nTests executing for REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
+#printf "\nTests executing for REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
 #printf "\nTests executing for IN-CONTEXT REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./forum/automation.js --feature=inContextRegistration >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
