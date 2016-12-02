2e61fed01a38fc66a89468845defe64d3b8846de
hotam <hotam@hotam.websitetoolbox.com>
2016-12-02 11:01:07 +0530

    merge demo code to check description

--- services/executorServices.js
+++ services/executorServices.js
@@ -33,8 +33,9 @@ executorServices.executeJob = function(commitDetails, callback){
 				var failTestResult = stdout.split(' ');
 				for(var i=0; i<failTestResult.length;i++) {
 					//failTestResult[i-1] = String(failTestResult[i-1]);
-					var fail = (failTestResult[i-1]).substring(20, 4);
+					//var fail = (failTestResult[i-1]).substring(20, 4);
 					if(failTestResult[i+1]=='tests') {
+						//var fail = failTestResult[i-1];
 						console.log('value ::::::::::: '+fail);
 						console.log('no of failed test case : '+failTestResult[i-1]);
 						descriptionRes = parseInt(descriptionRes)+parseInt(failTestResult[i]);
