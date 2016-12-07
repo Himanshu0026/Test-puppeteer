d0326012dedbde8e635d66e38e0f533a80398720
shipragoel123 <shipra@websitetoolbox.com>
2016-12-07 17:14:02 +0530

    Add inContext login feature.

--- bin/automation.sh
+++ bin/automation.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 AUTOMATION_HOME='/etc/automation'
 cd $AUTOMATION_HOME
-printf "Tests executing for LOGIN: \n" > "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=login >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
-#printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-#casperjs test ./automationScripts/automation.js --feature=incontextlogin >> "$AUTOMATION_HOME"/log/automation.txt
+#printf "Tests executing for LOGIN: \n" > "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=login >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
+printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+casperjs test ./automationScripts/automation.js --feature=inContextLogin >> "$AUTOMATION_HOME"/log/automation.txt
+sleep 1
 #printf "\nTests executing for REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./automationScripts/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
