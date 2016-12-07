88ff6a1ff1d1161cd9f3a9a37af6a3a6179a56a0
hotam <hotam@hotam.websitetoolbox.com>
2016-12-07 16:38:04 +0530

    merge code to attach sreenshot

--- services/mailServices.js
+++ services/mailServices.js
@@ -15,40 +15,22 @@ var smtpConfig = {
 var transporter = nodemailer.createTransport(smtpConfig);
  
 // send mail with defined transport object 
-fs.readdir("../automationScripts/failedScreenshots", function (err, data) {
-	if(err) {
-	
-	}else {
-		mailServices.sendMail = function(emailDetails, callback){
-			// setup e-mail data with unicode symbols 
-			var mailOptions = {
-				"from": 'noresponse@websitetoolbox.com', // sender address 
-				"to": emailDetails.committerEmail, // 'shipra@websitetoolbox.com',//list of receivers 
-				"subject": "Forum test result: "+emailDetails.branchName, // Subject line 
-				"text": 'Hello ' + emailDetails.committerName+",\n\n Following is the automation test result: \n"+emailDetails.testResult+"\nPlease find details of the automation test result attached herewith. \n" , // plaintext body 
-				"attachments": emailDetails.attachments //attachments
-			};
-			console.log("mailOptions : "+JSON.stringify(mailOptions));
-			//Initiating mail sending
-			transporter.sendMail(mailOptions, function(error, info){
-				if(error){
-					return callback(error);
-				}
-				console.log('Message sent: ' + info.response);
-				return callback();
-			});
-		};
-	}
-	mailer.send_mail({       
-	sender: 'sender@sender.com',
-	to: 'dest@dest.com',
-	subject: 'Attachment!',
-	body: 'mail content...',
-	attachments: [{'filename': 'attachment.txt', 'content': data}]
-	}), function(err, success) {
-	if (err) {
-	    // Handle error
-	}
-
-	}
-});
+mailServices.sendMail = function(emailDetails, callback){
+	// setup e-mail data with unicode symbols 
+	var mailOptions = {
+		"from": 'noresponse@websitetoolbox.com', // sender address 
+		"to": emailDetails.committerEmail, // 'shipra@websitetoolbox.com',//list of receivers 
+		"subject": "Forum test result: "+emailDetails.branchName, // Subject line 
+		"text": 'Hello ' + emailDetails.committerName+",\n\n Following is the automation test result: \n"+emailDetails.testResult+"\nPlease find details of the automation test result attached herewith. \n" , // plaintext body 
+		"attachments": emailDetails.attachments //attachments
+	};
+	console.log("mailOptions : "+JSON.stringify(mailOptions));
+	//Initiating mail sending
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			return callback(error);
+		}
+		console.log('Message sent: ' + info.response);
+		return callback();
+	});
+};
