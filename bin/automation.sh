5884145c86bb82940be91c26c3fab8299f692a58
shipragoel123 <shipra@websitetoolbox.com>
2016-12-08 11:48:32 +0530

    Made chnges in automation.sh

--- bin/automation.sh
+++ bin/automation.sh
@@ -4,18 +4,18 @@ cd $AUTOMATION_HOME
 printf "Tests executing for LOGIN: \n" > "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./automationScripts/automation.js --feature=login >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
-printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=inContextLogin >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
+#printf "\nTests executing for IN-CONTEXT LOGIN: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=inContextLogin >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
 #printf "\nTests executing for REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./automationScripts/automation.js --feature=register >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
 #printf "\nTests executing for IN-CONTEXT REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 #casperjs test ./forum/automation.js --feature=inContextRegistration >> "$AUTOMATION_HOME"/log/automation.txt
 #sleep 1
-printf "\nTests executing for BACKEND REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-casperjs test ./automationScripts/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
-sleep 1
+#printf "\nTests executing for BACKEND REGISTRATION: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#casperjs test ./automationScripts/automation.js --feature=backEndRegistration >> "$AUTOMATION_HOME"/log/automation.txt
+#sleep 1
 printf "\nTests executing for FORGOT PASSWORD: \n" >> "$AUTOMATION_HOME"/log/automation.txt
 casperjs test ./automationScripts/automation.js --feature=forgotPassword >> "$AUTOMATION_HOME"/log/automation.txt
 sleep 1
