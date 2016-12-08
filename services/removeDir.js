617008d0822c3d374871746c469f6ef16b69f0ec
hotam <hotam@hotam.websitetoolbox.com>
2016-12-08 16:50:56 +0530

    merge code to remove directory

--- services/removeDir.js
+++ services/removeDir.js
@@ -20,28 +20,3 @@ removeDir.deleteFolderRecursive = function(path) {
 		fs.rmdirSync(path);
 	}
 };
-
-//Method to Add Attachments
-removeDir.addAttacments = function(path) {
-	if( fs.existsSync(path) ) {
-		fs.readdirSync(path).forEach(function(file,index){
-			console.log('commitdetails : '+JSON.stringify(file));
-			/*var curPath = path + "/" + file;
-			if(fs.lstatSync(curPath).isDirectory()) { 
-				// recurse
-				removeDir.addAttacments(curPath);
-			} else { 
-				// delete file
-				//console.log('deleting file : '+curPath);
-				//fs.unlinkSync(curPath);
-				commitDetails['attachments'] = [
-					{
-						path: curPath
-					}
-				];
-			}*/
-		});
-		//console.log('deleting directory : '+path);
-		//fs.rmdirSync(path);
-	}
-};
