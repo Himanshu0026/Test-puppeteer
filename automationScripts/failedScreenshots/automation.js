e3ba8c3438b18a3d98d756af839d43893e6d46ea
hotam <hotam@hotam.websitetoolbox.com>
2016-12-08 16:47:10 +0530

    merge code to verify failedScreenshots directory

---
+++ automationScripts/failedScreenshots/automation.js
@@ -0,0 +1,70 @@
+var config = require("../config/config.json");
+
+casper.options.viewportSize = config.app.viewportSize;
+casper.options.verbose = config.app.verbose;
+casper.options.logLevel = config.app.logLevel;
+casper.options.waitTimeout = config.app.waitTimeout;
+
+var feature = casper.cli.get('feature');
+if(feature){
+	casper.echo("Started testing for the feature: " + feature +"\n",'INFO');
+}else{
+	casper.echo("It seems, you have not given any option.");
+}
+
+switch (feature) {
+    	
+    	case "login":
+		casper.test.begin('Verify login functionality from home page with all valid and invalid scenarios ', function(test) {
+			var forumLogin = require("./testsuite/main/login.js");
+			forumLogin.featureTest(casper, casper.test);
+			casper.run(function(){
+				test.done();
+			});
+		});
+	break;
+    	
+    	case "forgotPassword":
+		casper.test.begin('Verify forgot password functionality from home page with all valid and invalid scenarios ', function(test) {
+			var forgotPassword = require("./testsuite/main/forgotPassword.js");
+			forgotPassword.featureTest(casper, casper.test);
+			casper.run(function(){
+				test.done();
+			});
+		});
+	break;
+    		
+	case "backEndRegistration":
+		casper.test.begin('BACK END REGISTRATION TEST', function(test) {
+			var backEndRegister = require("./testsuite/main/backEndRegistration.js");
+			backEndRegister.featureTest(casper, casper.test);
+			casper.run(function(){
+				test.done();
+				test.assert(true);
+			});
+		});
+	break;
+    		
+	case "register":
+		casper.test.begin('REGISTRATION TEST', function(test) {
+			var forumRegister = require("./testsuite/main/register.js");
+			forumRegister.featureTest(casper, casper.test);
+			casper.run(function(){
+				test.done();
+				test.assert(true);
+			});
+		});
+	break;
+     
+	default:
+		casper.echo("Please select any feature from options given below. For ex: casperjs automation.js <option>.\n"); 
+        	casper.echo("Options:");
+		casper.echo("login");
+        	casper.echo("forgotPassword");
+		casper.echo("backEndRegistration");
+		casper.echo("register");
+		casper.exit();
+};
+
+
+
--- services/removeDir.js
+++ services/removeDir.js
@@ -1,18 +1,47 @@
 'use strict';
 var fs = require('fs');
 var removeDir = module.exports = {};
+var commitDetails = {};
+//Method to Delete Old Directory
 removeDir.deleteFolderRecursive = function(path) {
-  if( fs.existsSync(path) ) {
-    console.log('deleting files ');
-    fs.readdirSync(path).forEach(function(file,index){
-      var curPath = path + "/" + file;
-      if(fs.lstatSync(curPath).isDirectory()) { // recurse
-        removeDir.deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
-  console.log('directory is deleted successfully');
+	if( fs.existsSync(path) ) {
+		fs.readdirSync(path).forEach(function(file,index){
+			var curPath = path + "/" + file;
+			if(fs.lstatSync(curPath).isDirectory()) { 
+				// recurse
+				removeDir.deleteFolderRecursive(curPath);
+			} else { 
+				// delete file
+				console.log('deleting file : '+curPath);
+				fs.unlinkSync(curPath);
+			}
+		});
+		console.log('deleting directory : '+path);
+		fs.rmdirSync(path);
+	}
+};
+
+//Method to Add Attachments
+removeDir.addAttacments = function(path) {
+	if( fs.existsSync(path) ) {
+		fs.readdirSync(path).forEach(function(file,index){
+			console.log('commitdetails : '+JSON.stringify(file));
+			/*var curPath = path + "/" + file;
+			if(fs.lstatSync(curPath).isDirectory()) { 
+				// recurse
+				removeDir.addAttacments(curPath);
+			} else { 
+				// delete file
+				//console.log('deleting file : '+curPath);
+				//fs.unlinkSync(curPath);
+				commitDetails['attachments'] = [
+					{
+						path: curPath
+					}
+				];
+			}*/
+		});
+		//console.log('deleting directory : '+path);
+		//fs.rmdirSync(path);
+	}
 };
