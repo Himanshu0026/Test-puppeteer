c2081a599a8fdcb956ca87d63d8c7d354a153d3e
hotam <hotam@hotam.websitetoolbox.com>
2016-12-12 13:37:35 +0530

    merge code to delete a directory

--- services/executorServices.js
+++ services/executorServices.js
@@ -25,7 +25,7 @@ executorServices.executeJob = function(commitDetails, callback){
 			if(err) {
 				console.error("Error : "+err);
 			}else {
-				attachmentServices.deleteFolderRecursive('project/QA-automation/automationScripts/failedScreenshots');
+				attachmentServices.deleteFolderRecursive('/home/hotam/project/QA-automation/automationScripts/failedScreenshots');
 			}
 		});
 		//Executing automation test script
