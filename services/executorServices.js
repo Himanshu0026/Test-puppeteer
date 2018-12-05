b18f1955d9a88555943708cf7dbc55fe9edd9ed4
isneha <isneha@websitetoolbox.com>
2018-12-05 11:46:46 +0530

    done changes

--- services/executorServices.js
+++ services/executorServices.js
@@ -41,9 +41,12 @@ executorServices.executeJob = function(commitDetails, callback) {
 				//sqlConnection('INSERT INTO forums (uid, title, description, displayorder) VALUES ("116", "General", "General", "1")');
 				//sqlConnection('UPDATE settings SET post_approval=0 WHERE uid=116 ');
 				var values = sqlConnection('SELECT max(posts), userid,user FROM members WHERE uid="116" and user="hani";');
-				console.log("the type of values"+typeof(values)+" ,the values are"+values);
+				//var rows = JSON.parse(JSON.stringify(values));
+				//console.log("the type of values"+typeof(values)+" ,the values are"+values);
 				//sqlConnection('REPLACE INTO top_posters (uid,userid,posts) VALUE (12,26995029,1206)');
 				exec("/etc/automation/bin/oo_automation.sh " +commitDetails.branchName+ ' ' +commitDetails.commitId, function(code, stdout, stderr) {
+					var rows = JSON.parse(JSON.stringify(values));
+					console.log("the type of values"+typeof(values)+" ,the values are"+values);
 					console.log('Exit code : oo_automation : ', code);
 					console.log('Program output : oo_automation : ', stdout);
 					console.log('Program stderr: oo_automation : ', stderr);
