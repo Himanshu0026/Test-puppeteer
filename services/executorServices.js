3e1fce6fe1cb8812b24f171f48647e7c238bb5a5
isneha <isneha@websitetoolbox.com>
2018-12-14 19:44:56 +0530

    done some chnages

--- services/executorServices.js
+++ services/executorServices.js
@@ -153,7 +153,8 @@ executorServices.executeJob = function(commitDetails, callback) {
 							} else {
 							//Executing automation test script
 							console.log("Executing Automation Script For " + commitDetails.commitId + " CommitID");
-							exec("/etc/automation/bin/automation.sh | grep -E 'FAIL'" +commitDetails.branchName+ ' ' +commitDetails.commitId, function(code, stdout, stderr) {
+							//exec("/etc/automation/bin/automation.sh | grep -E 'FAIL'" +commitDetails.branchName+ ' ' +commitDetails.commitId, function(code, stdout, stderr) {
+							exec("/etc/automation/bin/automation.sh | grep -E 'FAIL'" , function(code, stdout, stderr) {
 							console.log('Exit code:', code);
 							console.log('Program output:', stdout);
 							console.log('Program stderr:', stderr);
