fa72c97fe08e7f0b3e583eb7f2bf04e15af36fa8
isneha <isneha@websitetoolbox.com>
2018-12-20 19:21:12 +0530

    done changes

--- services/executorServices.js
+++ services/executorServices.js
@@ -166,7 +166,7 @@ executorServices.executeJob = function(commitDetails, callback) {
 							//var testResult = '';
 							var testStdout = stdout;
 							var testResult1 = testStdout.replace(/\u001b\[.*?m/g, '');
-							var testResult = testResult1.split('\n');
+							var testResult2 = testResult1.split('\n');
 							var string = '';
 							var i;
 							for ( i = 1; i <= (testResult.length-2); i++) {
@@ -181,6 +181,7 @@ executorServices.executeJob = function(commitDetails, callback) {
 						    }
 								i++;
 							}
+							var testResult = string;
 							//var testResult2= testResult1.replace(/\nPASS/g, 'PASS');
 							//var testResult3 = testResult2.replace(/\nFAIL/g, 'FAIL');
 							/*var testResult4 = testResult3.split('\n').forEach(function(line) {
