5ca8c7157efcb2437aad02698bdbec9b1f6a497c
isneha <isneha@websitetoolbox.com>
2019-02-14 14:31:19 +0530

    done changes in the automation.sh

--- bin/automation.sh
+++ bin/automation.sh
@@ -167,14 +167,14 @@ then
   sleep 1
 fi
 
-checkAutomattionStatusToSkip
-
-if [ $AUTOMATION_ALLOW -eq '1' ]
-then
-  printf "Executing Automation Script For $1 commitID\nTests executing for MOVE TOPIC AND POST: \n" >> "$AUTOMATION_HOME"/log/automation.txt
-  casperjs test ./automationScripts/automation.js --feature="moveTopicAndPost" --branchName=$1 --commitId=$2>> "$AUTOMATION_HOME"/log/automation.txt
-  sleep 1
-fi
+# checkAutomattionStatusToSkip
+#
+# if [ $AUTOMATION_ALLOW -eq '1' ]
+# then
+#   printf "Executing Automation Script For $1 commitID\nTests executing for MOVE TOPIC AND POST: \n" >> "$AUTOMATION_HOME"/log/automation.txt
+#   casperjs test ./automationScripts/automation.js --feature="moveTopicAndPost" --branchName=$1 --commitId=$2>> "$AUTOMATION_HOME"/log/automation.txt
+#   sleep 1
+# fi
 
 checkAutomattionStatusToSkip
 
