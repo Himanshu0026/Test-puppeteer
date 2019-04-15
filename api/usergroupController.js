0e078e3cefb2185c5f4aa0626ee9c59986649550
isneha <isneha@websitetoolbox.com>
2019-04-15 16:41:35 +0530

    done changes

--- api/usergroupController.js
+++ api/usergroupController.js
@@ -30,4 +30,49 @@ routes.get("/enabledViewCategory", function(req, res, next) {
 	});
 });
 
+routes.get("/enabledStartTopic", function(req, res, next) {
+	var field = 'post_threads';
+	var value = '1';
+	var uid = '116';
+	var title = 'General';
+	sqlConnection(Usergroups.updateUsergroupsSQL(uid,field,value,title), function(err, result) {
+		if(!err) {
+			res.status(200).json({
+				message:"enabled the view category permission.",
+				usergroups:result
+			});
+		}
+	});
+});
+
+routes.get("/enabledReplyTopic", function(req, res, next) {
+	var field = 'other_post_replies';
+	var value = '1';
+	var uid = '116';
+	var title = 'General';
+	sqlConnection(Usergroups.updateUsergroupsSQL(uid,field,value,title), function(err, result) {
+		if(!err) {
+			res.status(200).json({
+				message:"enabled the view category permission.",
+				usergroups:result
+			});
+		}
+	});
+});
+
+routes.get("/enabledUploadAttachment", function(req, res, next) {
+	var field = 'upload_attachments';
+	var value = '1';
+	var uid = '116';
+	var title = 'General';
+	sqlConnection(Usergroups.updateUsergroupsSQL(uid,field,value,title), function(err, result) {
+		if(!err) {
+			res.status(200).json({
+				message:"enabled the view category permission.",
+				usergroups:result
+			});
+		}
+	});
+});
+
 module.exports = routes;
