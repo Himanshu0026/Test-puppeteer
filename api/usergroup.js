9368130d9e00c1feb3a1905606df88331fa8dca2
isneha <isneha@websitetoolbox.com>
2019-04-15 16:17:35 +0530

    done changes

--- api/usergroup.js
+++ api/usergroup.js
@@ -7,5 +7,6 @@ Usergroups.getAllUsergroupsSQL = function(uid) {
 
 Usergroups.updateUsergroupsSQL = function(uid,field,value,title) {
   var sql ='UPDATE usergroups SET '+field+ '=' +value+ 'WHERE uid = "'+uid+'" AND title ='+title+';';
+  console.log(sql);
   return sql;
 };
