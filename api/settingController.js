6e77e817c3cb27d049459cc816d76de0d414cb23
isneha <isneha@websitetoolbox.com>
2019-04-16 16:35:38 +0530

    done changes

--- api/settingController.js
+++ api/settingController.js
@@ -9,9 +9,10 @@ var settingRoutes = express.Router();
 settingRoutes.get("/getUID", function(req, res, next) {
 	sqlConnection(settings.getUID(user), function(err, result) {
 		if(!err) {
+      var uid = result[0].uid;
 			res.status(200).json({
 				message:"UID found.",
-				userID:result[0].uid
+				userID:uid
 			});
 		}
 	});
