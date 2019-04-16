308d653a8a98bbbb0ec1f79183b55a2d41c2c79e
isneha <isneha@websitetoolbox.com>
2019-04-16 12:47:47 +0530

    done changes

--- api/settingController.js
+++ api/settingController.js
@@ -3,10 +3,10 @@ var settings = require('./setting.js');
 var config = require('../config/config.json');
 var express = require('express');
 var app = express();
+var user = config.backendCred.uname;
 var settingRoutes = express.Router();
 
 settingRoutes.get("/getUID", function(req, res, next) {
-  var user = config.backendCred.uname;
 	sqlConnection(settings.getUID(user), function(err, result) {
 		if(!err) {
 			res.status(200).json({
