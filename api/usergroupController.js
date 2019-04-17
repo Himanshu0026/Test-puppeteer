b8e773db170de8d2892d1c00fa12eb443f17db52
isneha <isneha@websitetoolbox.com>
2019-04-17 10:19:06 +0530

    done changes

--- api/usergroupController.js
+++ api/usergroupController.js
@@ -63,16 +63,29 @@ routes.get("/enabledViewCategory/*", function(req, res, next) {
 });
 
 routes.get("/enabledStartTopic", function(req, res, next) {
-	var field = 'post_threads';
-	var value = '1';
-	var uid = '116';
-	var title = 'General';
-	sqlConnection(Usergroups.updateUsergroupsSQL(uid,field,value,title), function(err, result) {
-		if(!err) {
-			res.status(200).json({
-				message:"enabled the view category permission.",
-				usergroups:result
+	var groupTitle = req.url.split('/')[2];
+	var field = automationData.enabledViewCategory.field;
+	var value = automationData.enabledViewCategory.value;
+	request({
+		url: config.apiLocalUrl+'/settings/getUID',
+		json: true
+	}, function(err, response, body) {
+		if(err) {
+			console.log('err : '+err);
+			res.send(err);
+		}
+		if(response.statusCode == 200) {
+			uid = body.UID;
+			sqlConnection(Usergroups.updateUsergroupsSQL(uid,field,value,groupTitle), function(err, result) {
+				if(!err) {
+					res.status(200).json({
+						message:"enabled the start topic permission.",
+						usergroups:result
+					});
+				}
 			});
+		}else {
+			res.send('The uid not found');
 		}
 	});
 });
