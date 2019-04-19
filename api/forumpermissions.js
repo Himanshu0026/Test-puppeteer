f8bb1860d03ad3beaea0e05d5119dd6a4522f0d2
isneha <isneha@websitetoolbox.com>
2019-04-19 17:32:25 +0530

    done changes

--- api/forumpermissions.js
+++ api/forumpermissions.js
@@ -1,6 +1,7 @@
 var forumPermissions = module.exports = {};
 
 forumPermissions.updateForumPermissionsSQL = function(uid, forumid, usergroupID, field, value) {
-  var sql ='UPDATE forum_permissions SET '+field+ '=' +value+ ' WHERE uid = "'+uid+'" AND forumid ="'+forumid+'" AND usergroupid ="'+usergroupID+'";';
+  var sql ='UPDATE forum_permissions SET '+field+ '= "' +value+ '" WHERE uid = "'+uid+'" AND forumid ="'+forumid+'" AND usergroupid ="'+usergroupID+'";';
+  console.log(sql);
   return sql;
 };
