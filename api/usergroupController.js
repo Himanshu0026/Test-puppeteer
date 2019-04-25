3b742ece9e28e05ebbc39e0d0f7188eb0720b703
isneha <isneha@websitetoolbox.com>
2019-04-25 11:38:08 +0530

    done changes

--- api/usergroupController.js
+++ api/usergroupController.js
@@ -11,7 +11,7 @@ var routes = express.Router();
 
 routes.get("/getUsergroupID/:grouptitle(*)", function(req, res, next) {
 	var groupTitle = req.params.grouptitle;
-	caonsole.log('dgdjjjjjjjjjjjjjjjjj'+groupTitle);
+	console.log('dgdjjjjjjjjjjjjjjjjj'+groupTitle);
 	settings.setUID(function(err, uid) {
 		if(!err) {
 			uid = uid;
