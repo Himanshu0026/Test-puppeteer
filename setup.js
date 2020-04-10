eff328cd4bca55952b1751f6142c12004c14e974
Himanshu Kumar <HimanshuKumar@websitetoolbox.com>
2020-04-10 16:22:23 +0530

    add changes

--- setup.js
+++ setup.js
@@ -4,12 +4,13 @@ const fs = require('fs')
 const mkdirp = require('mkdirp')
 const os = require('os')
 const path = require('path')
+// const puppeteer = require('puppeteer-firefox');
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
 
 module.exports = async function() {
   console.log(chalk.green('Setup Puppeteer'))
-  const browser = await puppeteer.launch({})
+  const browser = await puppeteer.launch({PUPPETEER_PRODUCT : 'firefox'})
   // This global is not available inside tests but only in global teardown
   global.__BROWSER_GLOBAL__ = browser
   // Instead, we expose the connection details via file system to be used in tests
